e0dfe604d73fa9094a686d99c91b24dc08f8a943
Brian LaMere <brianlamere@gmail.com>
2025-09-27 20:42:40 -0700

    adding system_settings.R file for settings about the system you're using, not the project

---
+++ system_settings.R
@@ -0,0 +1,36 @@
+# Edit this file to match your local environment.
+# The settings in this file are very specific to installation and project!
+
+# Project source code directory
+Rmultiome_path <- "/projects/opioid/Rmultiome"
+
+# Working directory that has everything in it.
+project_base_dir <- "/projects/opioid"
+
+# If following guide precisely, nothing below to end of file would need to change.
+
+# Project raw data directory
+rawdatadir <- file.path(project_base_dir, "rawdata")
+
+# Vault of rds files saved at different milestones
+rdsdir <- file.path(project_base_dir, "vault")
+
+# Project output directory for reports, settings, etc.
+project_outdir <- file.path(project_base_dir, "project_export")
+
+# Project settings file
+project_settings_file <- file.path(project_outdir, "project_settings.R")
+
+# Reference files directory
+referencedir <- file.path(project_base_dir, "references")
+
+# Main 10X h5 file (RNA)
+h5filename <- "filtered_feature_bc_matrix.h5"
+
+# ATAC fragments file name
+atacfilename <- "atac_fragments.tsv.gz"
+
+# Reference annotation file
+referencefile <- "allen_m1c_2019_ssv4.rds"
+
+
